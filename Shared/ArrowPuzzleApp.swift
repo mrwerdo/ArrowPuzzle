740f1b79464150963dc4f78c4f0c59e7643bfc57
Andrew Thompson <6337878+Mrwerdo@users.noreply.github.com>
2022-07-03 12:52:07 +1000

    Initial Commit

---
+++ Shared/ArrowPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArrowPuzzleApp.swift
+//  Shared
+//
+//  Created by Andrew Thompson on 3/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct ArrowPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Andrew Thompson on 3/7/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
